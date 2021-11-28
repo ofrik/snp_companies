3abd85514bd5e81fb1e3063ef67a6b0437b286e9
Ofri Masad <ofrik89@gmail.com>
2021-11-28 18:28:29 +0200

    updated the setup

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='',
+    name='snp_companies',
     version='1.0',
     author="Ofri Masad",
     author_email="ofrik89@gmail.com",
-    description="TBD",
-    url="TBD",
+    description="Fetches and construct the S&P 500 based on the Wiki page",
+    url="https://github.com/ofrik/snp_companies",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
